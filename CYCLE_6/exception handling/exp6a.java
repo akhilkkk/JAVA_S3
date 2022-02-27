77616a25cc7f5f049d1d7074cf7899b953343d19
AKHIL-KRISHNAN <98377774+akhilkkk@users.noreply.github.com>
2022-02-27 14:29:54 +0000

    Add files via upload

---
+++ CYCLE_6/exception handling/exp6a.java
@@ -0,0 +1,18 @@
+
+
+class experiment6a
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            int b,a=5;
+            b=a/0;
+            System.out.println(b);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
